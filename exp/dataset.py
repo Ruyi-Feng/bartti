864fdf58f7b8cba2a42e74b8f31a8846f388af6e
Ruyi-Feng <fengruyi@seu.edu.cn>
2023-06-07 22:22:29 +0800

    feat: framework build

--- exp/dataset.py
+++ exp/dataset.py
@@ -1,16 +1,20 @@
 from torch.utils.data import Dataset
+from utils.noise import derive
 
 
-class Dataset_ETT_hour(Dataset):
+class Dataset_Bart(Dataset):
     def __init__(self):
         pass
         self.__read_data__()
 
     def __read_data__(self):
         pass
+        self.enc_x, self.dec_x, self.gt_x = derive(x)
+        # generate random mask here
 
     def __getitem__(self, index):
         pass
+    return enc_x, dec_x, gt_x
 
     def __len__(self):
         return len(self.data_x) - self.seq_len - self.pred_len + 1
--- models/bart.py
+++ models/bart.py
@@ -6,23 +6,22 @@ class Embedding(nn.Module):
     def __init__(self, config):
         super(Embedding, self).__init__()
         vocab_sz, self.d_model = config.vocab_sz, config.d_model
-        self._pos_embed = nn.Parameters(torch.rand(config.batch, config.d_model, config.seq_len))  # seq_len 也可以不固定
+        self._pos_embed = nn.Parameters(torch.rand(1, config.d_model, config.seq_len))  # seq_len 也可以不固定
         self._embeding = nn.Embedding(vocab_sz, self.d_model, padding_idx)
 
     def forward(self, x):
-        return self._embeding(x) + self._pos_embed(np.arrange(self.d_model))
+        return self._embeding(x) + self._pos_embed[:, :x.size(1)]
 
 
 class Bart(nn.Module):
     def __init__(self, config):
-        self.enc_embeding = Embedding(config)
-        self.dec_embeding = Embedding(config)
+        self.embeding = Embedding(config)
         self.bart = nn.Transformer(d_model=config.d_model,
                                    nhead=config.n_heads,
                                    num_encoder_layers=config.e_layers,
                                    num_decoder_layers=config.d_layers)
 
-    def forward(self, x):
-        enc_token = self.enc_embeding(x)
-        dec_token = self.dec_embeding(x)
+    def forward(self, x1, x2):
+        enc_token = self.embeding(x1)  # enc_x1 with noise
+        dec_token = self.embeding(x2)  # enc_x2 with noise
         self.bart(enc_token, dec_token)
