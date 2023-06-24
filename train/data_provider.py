0d04a97d6bd8614c46f9f16cd9bf9725a22455d6
Ruyi-Feng <fengruyi@seu.edu.cn>
2023-06-24 17:51:31 +0800

    fix: fixed head position update when create index

--- train/data_provider.py
+++ train/data_provider.py
@@ -33,15 +33,18 @@ class Data_Form:
             self._run(flnm, cols)
 
     def _run(self, flnm: str, cols: object):
-        data = pd.read_csv(self.flnms[flnm])
+        data = pd.read_csv(self.flnms[flnm][0])
         data = data.sort_values(by=cols.frame).reset_index(drop=True)
+        self.min_y = data[cols.top].min() - 10  # 用来把数据集的y规范到较小的范围
         f_data = open(".\data\data.bin", 'ab+')
         f_index = open(".\data\index.bin", 'ab+')
-        self.window.clear()  # 新flnm清空window
+        self.scale = self.flnms[flnm][1]
+        self._init_window()
         for ID, group in data.groupby(data[cols.frame]):
             byte_frame = 0
             for _, row in group.iterrows():
-                row_s = "{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f}\n".format(ID, row[cols.car_id], row[cols.left] + 0.5 * row[cols.width], row[cols.top] + 0.5 * row[cols.height], row[cols.width], row[cols.height])
+                id, cx, cy, w, h = self._stand_boxes(row, cols)
+                row_s = "{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f}\n".format(ID, id, cx, cy, w, h)
                 row_s = row_s.encode()
                 write_len = f_data.write(row_s)
                 byte_frame += write_len
@@ -49,6 +52,20 @@ class Data_Form:
             if notable:
                 idx_s = "{:s},{:d},{:d}\n".format(self._get_only_id(flnm, ID), head, tail).encode()
                 f_index.write(idx_s)
+
+    def _stand_boxes(self, row, cols):
+        id = row[cols.car_id]
+        cx = row[cols.left] + 0.5 * row[cols.width]
+        cy = row[cols.top] + 0.5 * row[cols.height] - self.min_y
+        w = row[cols.width]
+        h = row[cols.height]
+        id = id % 1024
+        cx, cy, w, h = cx * self.scale, cy * self.scale, w * self.scale, h * self.scale
+        return id, cx, cy, w, h
+
+    def _init_window(self):
+        self.last_bytes += self._sum_window()
+        self.window.clear()
 
     def _get_only_id(self, flnm, ID):
         return flnm + str(ID)
