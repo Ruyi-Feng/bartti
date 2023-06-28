c91ec0f49422e517986a692fc28d9db53ef297ce
Ruyi-Feng <fengruyi@seu.edu.cn>
2023-06-28 23:19:25 +0800

    fix: adjust parameter

--- train/params.py
+++ train/params.py
@@ -3,9 +3,9 @@ import torch
 
 def params():
     parser = argparse.ArgumentParser(description='Non-stationary Transformers for Time Series Forecasting')
-    parser.add_argument('--save_path', type=str, default='/output/', help='location of model checkpoints')
-    parser.add_argument('--index_path', type=str, default='/input0/index.bin')
-    parser.add_argument('--data_path', type=str, default='/input0/data.bin')
+    parser.add_argument('--save_path', type=str, default='/checkpoints/', help='location of model checkpoints')
+    parser.add_argument('--index_path', type=str, default='/data/index.bin')
+    parser.add_argument('--data_path', type=str, default='/data/data.bin')
     parser.add_argument('--interval', type=float, default=0.03)
 
     parser.add_argument('--is_train', type=bool, default=True, help='if True is train model')
@@ -22,22 +22,8 @@ def params():
     parser.add_argument('--d_layers', type=int, default=6, help='num of decoder layers')
 
     parser.add_argument('--activation', type=str, default='gelu', help='activation')
-
-    # parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
-    # parser.add_argument('--gpu', type=int, default='0', help='gpu')
-    # parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=True)
-    # parser.add_argument('--devices', type=str, default='0,1', help='device ids of multile gpus')
     parser.add_argument('--lradj', default='type1')
     args = parser.parse_args()
-    # args.use_gpu = (torch.cuda.is_available() and args.use_gpu)
-    # if args.use_gpu:
-    #     if args.use_multi_gpu:
-    #         args.devices = args.devices.replace(' ', '')
-    #         device_ids = args.devices.split(',')
-    #         args.device_ids = [int(id_) for id_ in device_ids]
-    #         args.gpu = args.device_ids[0]
-    #     else:
-    #         torch.cuda.set_device(args.gpu)
 
     return args
 
