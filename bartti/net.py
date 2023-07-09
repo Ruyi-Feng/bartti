49c55fabf898a1d43a0a350c8ae71bb39c79634f
Ruyi-Feng <fengruyi@seu.edu.cn>
2023-07-10 00:00:21 +0800

    fix: new embedding approach

--- bartti/net.py
+++ bartti/net.py
@@ -6,22 +6,21 @@ class Embedding(nn.Module):
         super(Embedding, self).__init__()
         self.d_model = config.d_model
         padding = 1 if torch.__version__ >= '1.5.0' else 2
-        self._pos_embed = nn.Parameter(torch.rand(1, config.max_seq_len, 1))  # 对于每个token的pos_embed是一样的
+        # self._pos_embed = nn.Parameter(torch.rand(1, config.max_seq_len, 1))  # 对于每个token的pos_embed是一样的
         # cov input x: [batch, seq_len, c_in]
-        self._embeding = nn.Conv1d(in_channels=config.c_in, out_channels=config.d_model,
+        self._token_embed = nn.Conv1d(in_channels=config.c_in, out_channels=config.d_model,
                                    kernel_size=3, padding=padding, padding_mode='circular', bias=False)
-        self._frm_embed = nn.Parameter(torch.rand(5, 1))
+        self._frm_embed = nn.Embedding(256, config.d_model, padding_idx=0)
+        self._car_embed = nn.Embedding(256, config.d_model, padding_idx=0)
 
-    def forward(self, x_pack, mark):
-        x, mk = x_pack
-        mks = mark[0]
-        for i in range(len(mks)):
-            if i == 0:
-                mk[0, :mks[i], 0] = self._frm_embed[i].item()
-                continue
-            mk[0, sum(mks[:i-1]):sum(mks[:i]), 0] = self._frm_embed[i].item()
-        # ! 这里需要检查维度是否正确
-        return self._embeding(x.permute(0, 2, 1)).transpose(1, 2) + self._pos_embed.data[:, :x.shape[1], :] + mk
+    def forward(self, x_group):
+        """
+        x_group: batch, seq_len, c_in
+        """
+        x = x_group[:, :, 2:]
+        f = x_group[:, :, 0]
+        c = x_group[:, :, 1]
+        return self._token_embed(x.permute(0, 2, 1)).transpose(1, 2) + self._frm_embed(f.int()) + self._car_embed(c.int())
 
 class Bart(nn.Module):
     def __init__(self, config):
@@ -34,13 +33,13 @@ class Bart(nn.Module):
                                    num_decoder_layers=config.d_layers,
                                    activation='gelu')
         padding = 1 if torch.__version__ >= '1.5.0' else 2
-        self.d_reduction = nn.Conv1d(in_channels=config.d_model, out_channels=config.c_in,
+        self.d_reduction = nn.Conv1d(in_channels=config.d_model, out_channels=config.c_out,
                                    kernel_size=3, padding=padding, padding_mode='circular', bias=False)
         self.criterion = nn.MSELoss()
 
-    def forward(self, enc_x, enc_mark, dec_x, dec_mark, gt_x, infer=False):
-        enc_token = self.enc_embeding(enc_x, enc_mark)
-        dec_token = self.dec_embeding(dec_x, dec_mark)
+    def forward(self, enc_x, dec_x, gt_x, infer=False):
+        enc_token = self.enc_embeding(enc_x)
+        dec_token = self.dec_embeding(dec_x)
         tgt_mask = None
         if not infer:
             tgt_mask = nn.Transformer.generate_square_subsequent_mask(
