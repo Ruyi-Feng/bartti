41f6af187e8c4007f7c0351085cdde43601e7930
Ruyi-Feng <fengruyi@seu.edu.cn>
2023-07-23 16:09:49 +0800

    feat: pratical main of finetune

---
+++ main_finetune.py
@@ -0,0 +1,12 @@
+from finetune.exp import Exp_Ft
+from finetune.params import params
+import os
+
+
+if __name__ == '__main__':
+    args = params()
+    print("---------init finetune main---------------")
+    os.environ['NCCL_ASYNC_ERROR_HANDLING'] = '1'
+    local_rank = int(os.environ['LOCAL_RANK'])
+    bartti = Exp_Ft(args, local_rank)
+    bartti.train()
