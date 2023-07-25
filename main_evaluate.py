22c433c19d7b88e68705b63148099e760c40f960
Ruyi-Feng <fengruyi@seu.edu.cn>
2023-07-25 23:52:54 +0800

    feat: add evaluation main

--- main_evaluate.py
+++ main_evaluate.py
@@ -3,9 +3,19 @@ import os
 
 
 if __name__ == '__main__':
-    os.environ['NCCL_ASYNC_ERROR_HANDLING'] = '1'
-    local_rank = int(os.environ['LOCAL_RANK'])
-    for tsk in ["compensation", "prediction", "simualtion"]:
-        Evaluation(local_rank, if_pretrain=True, task=tsk, check_list=["overlap"], rslt_path="./results/"+tsk+"/rslt.json")
+    # os.environ['NCCL_ASYNC_ERROR_HANDLING'] = '1'
+    # local_rank = int(os.environ['LOCAL_RANK'])
+
+    # for tsk in ["simulation"]:  # , 
+    #     rslt_pth = "./results/pretrain_enc6_dec6/"+tsk+"/rslt.json"
+    #     eva = Evaluation(if_pretrain=True, task=tsk)
+    #     eva.obtain_results(rslt_path=rslt_pth, local_rank=local_rank)
+
+    for tsk in ["prediction", "simulation", "compensation", ]:
+        rslt_pth = "./results/pretrain_enc6_dec6/"+tsk+"/rslt.json"
+        eva = Evaluation(if_pretrain=True, task=tsk)
+        print("----------start check results-----------")
+        eva.check_results(check_list=["overlap"], rslt_path=rslt_pth)
+
 
 
